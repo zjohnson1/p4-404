9078ff6f008ca3159a7d1c7b3b900122ba796ae7
zjohnson1 <zjohnson@eagles.ewu.edu>
2014-11-18 09:58:11 -0800

    version 9

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,3 @@
 $().ready(function() {
-		$('#wrapper').tubular({videoId: 'Ljm4Uv0KCSQ'}); // where idOfYourVideo is the YouTube ID.
+		$('#wrapper').tubular({videoId: 'Ljm4Uv0KCSQ?'}); // where idOfYourVideo is the YouTube ID.
 	});
